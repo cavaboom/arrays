440ab6a1a6fc7b1688a3e1df32b1dddae66fa214
dendariev <den.dariev@gmail.com>
2022-12-04 13:16:30 +0800

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hellow world");
+        System.out.println("Задание 1");
+        int[] array1 = new int [3];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+
+        double[] array2 = {1.57, 7.654, 9.986};
+
+        byte[] array3 = {33, 52, 120};
     }
 }
