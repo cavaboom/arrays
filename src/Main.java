22a744b6de38cbbe667bb0e3f63aeb4ab2c9962b
dendariev <den.dariev@gmail.com>
2022-12-04 13:44:50 +0800

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,27 @@ public class Main {
         double[] array2 = {1.57, 7.654, 9.986};
 
         byte[] array3 = {33, 52, 120};
+        System.out.println("Задание 2");
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i]);
+            if (i != array1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print(array2[i]);
+            if (i != array1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < array3.length; i++) {
+            System.out.print(array3[i]);
+            if (i != array1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 }
